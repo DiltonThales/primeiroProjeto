a32e20ac115662725552bb097634e51f34582016
dilton <diltonthales.dt@gmail.com>
2026-03-01 16:58:58 -0300

    feat: adicao conversão de tipos

---
+++ src/ConversaoDeTipos.java
@@ -0,0 +1,26 @@
+public class ConversaoDeTipos {
+    public static void main(String[] args) {
+        int numero = 10;
+        double d = numero;
+
+        double d2 = 10.5;
+        int int2 = (int) d2;
+
+        Integer numeroInteger = 10;
+        String numeroString = numeroInteger.toString();
+
+        Long numeroLong = 10L;
+        String numeroStringLong = numeroLong.toString();
+
+        String numeroStringDois = "1234";
+        Integer numeroConvertido = Integer.parseInt(numeroStringDois);
+        Double numeroConvertidoDouble = Double.parseDouble(numeroStringDois);
+        Long numeroConvertidoLong = Long.parseLong(numeroStringDois);
+
+
+        System.out.println(d);
+        System.out.println(int2);
+        System.out.println(numeroString);
+        System.out.println(numeroConvertido);
+    }
+}
