f1fe7b04efa0af9b4747456434e6dc64d2797d41
dilton <diltonthales.dt@gmail.com>
2026-03-01 16:20:17 -0300

    feat: adicao operacdores e trabalhando com strings

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String g = "dilton";
+        int idade = 25;
+        long qtdCachorros = 10L;
+        double salario = 4500.00;
+        char genero = 'M';
+        boolean empreagoJava = true;
+
+        System.out.println("idade: " + idade);
+        System.out.println("qtd cachorros: " + qtdCachorros);
+        System.out.println("Salário: " + salario);
+        System.out.println("genero:  " + genero);
+        System.out.println("empregado: " + empreagoJava);
+
+
+    }
+}
