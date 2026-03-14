9b07de1c9ca7ba9ee7c6f53e026152975807f353
dilton <diltonthales.dt@gmail.com>
2026-03-14 16:59:53 -0300

    refactor: ajustes

--- src/contabancaria/Agencia.java
+++ src/contabancaria/Agencia.java
@@ -13,5 +13,12 @@ public class Agencia {
         System.out.println("O titular da conta é " + titularConta + " e o saldo final é " + saldoFinal);
 
 
+
+
+        ContaBancaria contaDaMinhaMae = new ContaBancaria("Maria");
+        System.out.println("O titular da conta é " + contaDaMinhaMae.getTitular() + " e o saldo final é " + contaDaMinhaMae.getSaldo());
+
+        //Isso que é o encapsulamento, sempre começa os atributos com private e acessa os artibutos através dos métodos que são os get e setter e construtores com atributos ou sem atributos para inicializar o objeto.
+
     }
 }
