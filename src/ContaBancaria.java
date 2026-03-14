ca219fb5b5e253dd5344614cb9cd3831bd38a92a
dilton <diltonthales.dt@gmail.com>
2026-03-14 16:33:28 -0300

    feat: adicao ContaBancaria

---
+++ src/ContaBancaria.java
@@ -0,0 +1,24 @@
+public class ContaBancaria {
+    //Atributos privados
+
+    private String titular;
+    private Double saldo;
+
+    //Getter e Setter = Métodos padrões para acessar e modificar os ateributos
+
+    //MEtodo par4a obter o titular
+    public String getTitular() {
+        return titular;
+    }
+
+    //Metodo para modificar o titulareeeeeee
+    public void setTitular(String novoTitular) {
+        this.titular = novoTitular; //this. se refere ao atributo titular, como padrão é criado como titular, quando me refiro ao atributo usa o titular e usa o this, e quando estou me referindo ap parametro e seomente o titular.
+
+    }
+
+
+
+
+
+}
