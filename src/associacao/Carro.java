2b03dd6b94edd074921d87a4b20471a56cfe687b
dilton <diltonthales.dt@gmail.com>
2026-03-23 20:23:43 -0300

    feat: fez uma associação, pois a classe motor dentro da classe carro, e temos um atributo do tipo motor

---
+++ src/associacao/Carro.java
@@ -0,0 +1,27 @@
+package associacao;
+
+public class Carro {
+
+    //atributo tipo motor
+    private Motor motor;
+
+    public Carro(Motor motor) {
+        this.motor = motor;
+    }
+
+    public Carro(int cilidradas){
+        this.motor = new Motor(cilidradas);
+    }
+
+    public void mostrarCilidradas(){
+        System.out.println("Carro com motor de " + motor.getCilidradas() + " cilidradas");
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
+    }
+}
---
+++ src/associacao/Principal.java
@@ -0,0 +1,8 @@
+package associacao;
+
+public class Principal {
+    public static void main(String[] args) {
+        Carro fusca = new Carro(160);
+        fusca.mostrarCilidradas();
+    }
+}
