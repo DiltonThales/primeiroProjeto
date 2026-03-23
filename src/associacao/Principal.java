4798b84eff337a35aa86546051235a1caeb59527
dilton <diltonthales.dt@gmail.com>
2026-03-23 20:37:04 -0300

    feat: Agregação,pois contrui um objeto com uma lista de funcionarios, e tambem é possivel criar  um objeto sem a lista de funcionario

--- src/associacao/Principal.java
+++ src/associacao/Principal.java
@@ -1,8 +1,30 @@
 package associacao;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Principal {
     public static void main(String[] args) {
         Carro fusca = new Carro(160);
         fusca.mostrarCilidradas();
+
+
+        Funcionarios joao = new Funcionarios("joão");
+        Funcionarios joana = new Funcionarios("Joana");
+        Funcionarios mateus = new Funcionarios("Masteus");
+
+        List<Funcionarios> listaDeFuncionarios = new ArrayList<>();
+        listaDeFuncionarios.add(joao);
+        listaDeFuncionarios.add(joana);
+        listaDeFuncionarios.add(mateus);
+
+
+        Departamento ti = new Departamento("TI", listaDeFuncionarios);
+        ti.mostrarFuncinarios();
+
+        Departamento marketing = new Departamento("Marketing", Collections.emptyList());
+        marketing.mostrarFuncinarios();
+
     }
 }
